e6142de056cb513438bf60f4c83f7e44272229c1
LilyBurton <lily.burton1992@gmail.com>
2024-08-28 16:13:34 +0100

    continuing learning about web server

--- server.js
+++ server.js
@@ -12,6 +12,18 @@ const myEmitter = new Emitter();
 
 const PORT = process.env.PORT || 3500
 
+const serveFile = async (filePath, contentType, response) => {
+    try {
+        const data = await fsPromises.readFile(filePath, 'utf8');
+        response.writeHead(200, { 'Content-Type': contentType });
+        response.end(data)
+    } catch (err) {
+        console.log(err)
+        response.statusCode = 500
+        response.end()
+    }
+}
+
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
@@ -40,7 +52,35 @@ const server = http.createServer((req, res) => {
             break;
         default:
             contentType = 'text/html';
-        
+
+        let filePath =
+            contentType === 'text/html' && req.url === '/'
+            ? path.join(__dirname, 'views', 'index.html')
+            : contentType === 'text/html' && req.url.slice(-1) === '/'
+                ? path.join(__dirname, 'views', req.url, 'index.html')
+                : contentType === 'index.html'
+                    ? path.join(__dirname, 'views', req.url)
+                    : path.join(__dirname, req.url);
+
+        if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+        const fileExists = fs.existsSync(filePath);
+
+        if (fileExists) {
+
+        } else {
+            switch(path.parse(filePath).base) {
+                case 'old-page.html':
+                    res.writeHead(301, { 'Location': '/new-page.html' });
+                    res.end();
+                    break;
+                case 'www-page.html':
+                    res.writeHead(301, { 'Location': '/' });
+                    res.end();
+                default:
+                    // a 404 response
+            }
+        }
     }
 });
 
